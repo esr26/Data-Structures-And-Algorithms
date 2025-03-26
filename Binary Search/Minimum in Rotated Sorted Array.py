e54296870c4a6a5ae5d4f3b39e2ef9760deb0139
Rajkumar Errakutti Sivakumar <19406828+esr26@users.noreply.github.com>
2025-03-26 10:56:16 +0530

    More optimal

--- Binary Search/Minimum in Rotated Sorted Array.py
+++ Binary Search/Minimum in Rotated Sorted Array.py
@@ -1,24 +1,16 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        left = 0
-        right = len(nums)-1
+
+        left, right = 0, len(nums)-1
 
         while left < right:
-            mid = left + (right-left)//2
-            if nums[left] >= nums[mid]:
-                if nums[mid] < nums[right]:
-                    right = mid
-                else:
-                    left = mid + 1
+            mid = (left+right)//2
+
+            if nums[mid] > nums[right]:
             
-            elif nums[mid] > nums[right]:
-                if nums[mid] > nums[left]:
-                    left = mid + 1
-                else:
-                    right = mid
+                left = mid + 1
             else:
-                return nums[left]
-        return nums[left]
+                right = mid
 
-
+        return nums[left] 
         
