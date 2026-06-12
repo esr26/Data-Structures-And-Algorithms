b4fa41d2337da8826ca64b6c08072faf69851f57
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-06-12 10:57:51 +0530

    dsa two pointers
    dsa two pointers medium and hard level

---
+++ Two Pointers/container with most water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        res = 0
+
+        left = 0
+        right = len(height)-1
+
+        while left < right:
+
+            res = max(res, min(height[left],height[right])*(right-left))
+
+            if height[right] > height[left]:
+                left += 1
+            
+            else:
+                right -= 1
+        
+        return res
+
+        
