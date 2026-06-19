1474c80d4806b43a7577a1df2e21347e19e2b9de
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-06-19 12:10:06 +0530

    dsa
    stack problem asteroid collision

---
+++ Stacks/asteroid collision.py
@@ -0,0 +1,28 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        
+        stack = []
+
+        for num in asteroids:
+
+            alive = True
+
+            while alive and stack and stack[-1] > 0 and num < 0:
+
+                if stack[-1] < abs(num):
+                    stack.pop()
+                
+                elif stack[-1] == abs(num):
+                    stack.pop()
+                    alive = False
+                
+                else:
+                    alive = False
+            
+            if alive:
+                stack.append(num)
+
+
+
+
+        return stack
