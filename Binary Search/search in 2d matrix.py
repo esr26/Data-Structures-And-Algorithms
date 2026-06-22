28bdf76c25f32cf09319cbe6bc6f5d4a39bfa844
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-06-22 11:30:18 +0530

    binary search
    search in 2d matrix solved

---
+++ Binary Search/search in 2d matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        left = 0
+        right = rows * cols - 1
+
+        while left <= right:
+
+            mid = (left + right) // 2
+
+            row = mid // cols
+            col = mid % cols
+
+            val = matrix[row][col]
+
+            if val == target:
+                return True
+
+            elif val < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return False
