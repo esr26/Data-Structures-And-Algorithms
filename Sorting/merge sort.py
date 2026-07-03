a04f46c41e10ae53db5eb19edc8f26fdb870ee51
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-07-04 05:18:39 +0530

    sorting
    merge sort implementation

---
+++ Sorting/merge sort.py
@@ -0,0 +1,44 @@
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+
+    result = []
+
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+
+arr = [8, 4, 2, 6, 1, 5, 3, 7]
+
+print(merge_sort(arr))
+
