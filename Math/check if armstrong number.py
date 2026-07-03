6bdc4ec8907553f3c106782da886d1a6569d9fcd
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-07-04 03:58:30 +0530

    math
    check if armstrong number solved

---
+++ Math/check if armstrong number.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def countDigits(self, num):
+        if num == 0:
+            return 1
+
+        count = 0
+        while num:
+            count += 1
+            num //= 10
+        return count
+
+    def isArmstrong(self, n: int) -> bool:
+        power = self.countDigits(n)
+        total = 0
+        temp = n
+
+        while temp:
+            digit = temp % 10
+            total += digit ** power
+            temp //= 10
+
+        return total == n
+        
