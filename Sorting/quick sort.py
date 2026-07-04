05c55b6d205c6e737a3309a5313fc0ff74f82762
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-07-04 05:48:04 +0530

    sorting
    quick sort implementation

---
+++ Sorting/quick sort.py
@@ -0,0 +1,33 @@
+def partition(arr, low, high):
+
+    pivot = arr[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+
+    return i + 1
+
+
+def quick_sort(arr, low, high):
+
+    if low < high:
+
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)
+
+        quick_sort(arr, pi + 1, high)
+
+
+arr = [8, 4, 7, 2, 1, 5, 3]
+
+quick_sort(arr, 0, len(arr) - 1)
+
+print(arr)
