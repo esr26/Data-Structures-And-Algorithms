32b36d3e0a32d898699757939bfa1bf3c4ba989f
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-07-05 12:37:58 +0530

    array
    remove duplicates from array solved

---
+++ Array & Hashing/remove duplicates from array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        slow = fast = 0
+
+        n = len(nums)
+
+        while fast < n:
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+            
+            fast += 1
+        
+        return slow + 1
+        
