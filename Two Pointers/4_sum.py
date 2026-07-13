1cd823c9acc4fd388ad338811799626fc650ed59
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-07-14 04:05:50 +0530

    updated 4 sum
    two pointer problem solved

--- Two Pointers/4_sum.py
+++ Two Pointers/4_sum.py
@@ -1,19 +1,17 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-
         nums.sort()
-        res = []
         n = len(nums)
+        ans = []
 
+        for i in range(n - 3):
 
-        for i in range(n):
-
-            if i > 0 and nums[i-1]==nums[i]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            for j in range(i+1, n):
+            for j in range(i + 1, n - 2):
 
-                if j > i+1 and nums[j-1] == nums[j]:
+                if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
 
                 left = j + 1
@@ -23,26 +21,22 @@ class Solution:
 
                     total = nums[i] + nums[j] + nums[left] + nums[right]
 
-                    if total > target:
+                    if total < target:
+                        left += 1
 
+                    elif total > target:
                         right -= 1
 
-                    elif total < target:
-                        left += 1
-                    
                     else:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
 
                         left += 1
                         right -= 1
 
-                        while left < right and nums[left-1] == nums[left]:
+                        while left < right and nums[left] == nums[left - 1]:
                             left += 1
-                        
-                        while left < right and nums[right+1] == nums[right]:
+
+                        while left < right and nums[right] == nums[right + 1]:
                             right -= 1
-        
-        return res
 
-
-        
+        return ans
