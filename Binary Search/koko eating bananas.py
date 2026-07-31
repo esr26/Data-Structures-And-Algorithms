e2a650a42dbe2ef6b53b7c371c4baa08e81ab499
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-08-01 04:46:31 +0530

    updated
    binary search koko eating bananas

--- Binary Search/koko eating bananas.py
+++ Binary Search/koko eating bananas.py
@@ -1,11 +1,12 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-
+        
         left = 1
         right = max(piles)
 
+
         while left <= right:
-            mid = (left+right)//2
+            mid = (left + right) // 2
 
             hours = 0
 
@@ -13,10 +14,11 @@ class Solution:
                 hours += (pile + mid - 1) // mid
             
             if hours <= h:
+                ans =mid
                 right = mid - 1
             
             else:
                 left = mid + 1
         
         return left
-        
+                
