1de0aea89ec63ede1543c3dc4d6944c9c2bf82b9
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-08-13 05:13:45 +0530

    binary search
    split largest sum solved

--- Binary Search/split-array-largest-sum.py
+++ Binary Search/split-array-largest-sum.py
@@ -1,29 +1,30 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        
+
         left = max(nums)
         right = sum(nums)
 
-        while left <= right:
-            mid = (left+right)//2
 
-            curr_sum = 0
+        while left <= right:
+            mid = (left + right) // 2
 
             subarray = 1
+            curr = nums[0]
 
-            for num in nums:
-
-                if curr_sum + num > mid:
+            for num in nums[1:]:
+                if curr + num > mid:
                     subarray += 1
-                    curr_sum = num
+                    
+                    curr = num
                 
                 else:
-                    curr_sum += num
+                    curr += num
             
             if subarray <= k:
                 right = mid - 1
             
             else:
                 left = mid + 1
-        
+
         return left
+
