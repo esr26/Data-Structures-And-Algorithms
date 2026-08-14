a23161aae715bfdced6e4720c37889829dd8afe6
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-08-15 04:43:16 +0530

    search-a-2d-matrix
    updated

--- Binary Search/search in 2d matrix.py
+++ Binary Search/search in 2d matrix.py
@@ -1,28 +1,25 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-
-        rows = len(matrix)
-        cols = len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
 
         left = 0
-        right = rows * cols - 1
+        right = m * n - 1
 
         while left <= right:
-
             mid = (left + right) // 2
 
-            row = mid // cols
-            col = mid % cols
+            row = mid // n
+            col = mid % n
 
-            val = matrix[row][col]
-
-            if val == target:
+            if matrix[row][col] == target:
                 return True
 
-            elif val < target:
+            elif matrix[row][col] < target:
                 left = mid + 1
 
             else:
                 right = mid - 1
 
         return False
+
