ab600dcdb3fe668e3fdf262a33713350db2ab53e
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-08-19 05:37:28 +0530

    string and dictionary
    Roman to Integer problem solved

---
+++ Strings/roman to integer.py
@@ -0,0 +1,29 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        i = 0
+        res = 0
+
+        while i < len(s):
+
+            if i + 1 < len(s) and roman[s[i + 1]] > roman[s[i]]:
+                res += roman[s[i + 1]] - roman[s[i]]
+                i += 2
+
+            else:
+                res += roman[s[i]]
+                i += 1
+
+        return res
+
+        
