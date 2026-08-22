f58c34921fd23b3029d8c75c58e4b217b7ade285
Rajkumar Errakutti Sivakumar <rajerrasiva@gmail.com>
2026-08-22 06:09:28 +0530

    string problem
    longest common prefix solved

--- Array & Hashing/longest common prefix.py
+++ Array & Hashing/longest common prefix.py
@@ -1,16 +1,13 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        s = strs[0]
+        for i in range(len(strs[0])):
 
-        for word in strs[1:]:
+            for s in strs:
 
-            while not word.startswith(s):
+                if i == len(s) or s[i] != strs[0][i]:
+                    return strs[0][:i]
 
-                s = s[:-1]
+        return strs[0]
 
-                if not s:
-                    return ""
-        
-        return s
-        
+    
